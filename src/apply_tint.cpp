e48c07bd92f37176cd2ae7cb9b1c7ccd99689709
femifacia <femi.facia@epitech.eu>
2023-04-19 09:03:25 +0100

    [ADD] ressources directory containing test photos; [ADD] source code to read an image in the main.cpp file

---
+++ src/apply_tint.cpp
@@ -0,0 +1,7 @@
+/*
+** EPITECH PROJECT, 2023
+** apply_tint_for_an_image
+** File description:
+** apply_tint for an image of course
+*/
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <opencv2/opencv.hpp>
 #include <stdio.h>
 #include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 int main(int argc, char **argv)
 {
+	cv::Mat image = cv::imread(argv[1]);
+
+	cv::imshow("Original", image);
+	cv::imshow("With tint", image);
+	cv::waitKey(0);
 	return (0);
 }
